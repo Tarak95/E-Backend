d6ab37abfe0aa4815a978291870dd5f6f354cf5b
Tarak <tarek8295@gmail.com>
2026-05-03 19:32:18 +0600

    New update added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const dbConfig = () => {
-    
-    mongoose.connect("mongodb+srv://E-Backend:meHztp5m3C8lC7yT@cluster0.ryyhaiz.mongodb.net/ecobazar?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => {
-        console.log("Database Connected");
-    })
-    
-}
+    mongoose.connect("mongodb+srv://E-Backend:meHztp5m3C8lC7yT@cluster0.ryyhaiz.mongodb.net/ecobazar?retryWrites=true&w=majority")
 
-module.exports = dbConfig
+    .then(() => {
+        console.log(" Database Connected");
+    })
+   
+};
+
+module.exports = dbConfig;
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
+
 const express = require("express")
 const app = express()
+
+
 
 const cors = require("cors")
 const dbConfig = require("./config/dbConfig")
@@ -19,6 +23,9 @@ app.get("/",(req,res) =>{
     res.send("Hello Bangladesh")
 })
 
-app.listen(8000,() =>{
-    console.log("Server is running on port 8000")
-})
+let port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); 
+});
+
