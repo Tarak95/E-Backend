0b433ba8e459156053e3cca41d69e4f222e828dc
Tarak <tarek8295@gmail.com>
2026-05-09 18:34:29 +0600

    New update added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+require('node:dns').setServers(['1.1.1.1', '8.8.8.8']);
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const dbConfig = require("./config/dbConfig")
-// const User = require('./models/userModel')
-// const jwt = require('jsonwebtoken')
-// const {registrationController} = require('./controllers/authenticationController')
 const {registrationController,loginController,forgotPasswordController,resetPasswordController,resendVerificationEmailController,verifyEmailController} = require('./controllers/authenticationController')
+const { getAllUsersController, singleUserDataController, deleteUserController, updateUserController } = require('./controllers/userController');
 
 
 
@@ -21,11 +21,22 @@ app.post("/registration", registrationController);
 app.post("/login", loginController);
 app.post("/forgotpassword", forgotPasswordController);
 app.post("/resetpassword/:token", resetPasswordController);
-// app.post("/resendveryfication", resendVerificationEmailController);
-
 app.post("/resendVerificationEmail", resendVerificationEmailController);
-
 app.post('/verifyemail/:token', verifyEmailController)
+
+
+
+
+
+
+// User management
+
+
+app.get('/allusers', getAllUsersController)
+app.get('/singleuser/:id', singleUserDataController)
+app.delete('/deleteuser/:id', deleteUserController)
+app.post('/update/:id', updateUserController)
+
 
 
 let port = process.env.PORT || 5000
